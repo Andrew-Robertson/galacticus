721bdeeed591349837a4048ee3d54d50222cf473
github-action-benchmark <github@users.noreply.github.com>
2025-09-28 02:53:41 +0000

    add Dark matter-only subhalos benchmarks (COZMIC WDM 3keV) (customSmallerIsBetter) benchmark result for 7362ac5d86461b0509740066764812355dc1cee1

---
+++ dev/bench/darkMatterOnlySubhalosCOZMICWDM3keVMilkyWay/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1759028021330,
+  "repoUrl": "https://github.com/Andrew-Robertson/galacticus",
+  "entries": {
+    "Dark matter-only subhalos benchmarks (COZMIC WDM 3keV)": [
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "7362ac5d86461b0509740066764812355dc1cee1",
+          "message": "Merge pull request #929 from galacticusorg/depup/lesgourg/class_public\n\nfix(deps): update lesgourg/class_public to 3.3.3",
+          "timestamp": "2025-09-26T17:20:45Z",
+          "tree_id": "e546fd083ca552709c3f85824a3c634d234104ff",
+          "url": "https://github.com/Andrew-Robertson/galacticus/commit/7362ac5d86461b0509740066764812355dc1cee1"
+        },
+        "date": 1759028020424,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos (COZMIC WDM 3keV Milky Way) - Likelihood - subhaloMassFunction",
+            "value": 3.6921722527360084,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos (COZMIC WDM 3keV Milky Way) - Likelihood - subhaloRadialDistribution",
+            "value": 3.8134672700844345,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos (COZMIC WDM 3keV Milky Way) - Likelihood - subhaloVelocityMaximumMean",
+            "value": 142.59537972973553,
+            "unit": "-logℒ"
+          }
+        ]
+      }
+    ]
+  }
+}
